218333ed707f0308b3603f9e0a4a0c1f424f4993
Richie Roman <answer417@gmail.com>
2021-04-17 07:55:16 -0700

    add server and model

---
+++ models/workout.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    day: { type: Date, default: () => new Date() },
+    exercises: [
+      {
+        type: {
+          type: String,
+        },
+        name: {
+          type: String,
+        },
+        duration: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+workoutSchema.virtual("totalDuration").get(function() {
+  return this.exercises.reduce(function (total, exercise) {
+      return total + exercise.duration;
+    }, 0);
+});
+
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
